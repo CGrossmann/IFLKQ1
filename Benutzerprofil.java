bdcc5ba97107d660d3fc6a300dc1a770bb99bc3e
Cornelius <corneliusgrossmann@hotmail.com>
2017-05-15 15:07:22 +0200

    Gemeinsames Projekt angelegt

---
+++ Benutzerprofil.java
@@ -0,0 +1,18 @@
+public class Benutzerprofil implements ComparableContent<Benutzerprofil>{
+    private String benutzername;
+    private String passwrort;
+    public Benutzerprofil(String login, String pPasswort){
+        benutzername = login;
+        passwort = pPaswort;
+        
+    }
+    public booleam isLess(Benutzerprofil pProfil){
+        return(pProfil.
+    }
+    public booleam isEqual(Benutzerprofil pProfil){
+        
+    }
+    public booleam isGreater(Benutzerprofil pProfil){
+        
+    }
+}
